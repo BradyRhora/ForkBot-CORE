82701992d795592fc8cb9d230ad2132f51798bd8
Brady Rhora <brady0423@gmail.com>
2023-03-14 15:01:46 -0400

    ppleae

--- ForkBot CORE/Classes/Command Classes/Commands.cs
+++ ForkBot CORE/Classes/Command Classes/Commands.cs
@@ -2231,7 +2231,7 @@ namespace ForkBot
             {
                 var request = new CompletionCreateRequest()
                 {
-                    Prompt = $"Here is a question marked as Q, and the answer to the question marked as A.\nQ:{input}\nA:",
+                    Prompt = $"Here is a prompt marked with Q and the answer/result to the prompt marked as A.\nQ:{input}\nA:",
                     MaxTokens = Math.Min(Stevebot.Chat.MAX_USER_TOKENS - userTokenCount, 256),
                     Temperature = 0.7f
                 };
