21294ddd97c08f40b7561e8e9227a4326879d998
Brady Rhora <brady0423@gmail.com>
2023-03-21 00:54:47 -0400

    fix dum stoopid

--- ForkBot CORE/Classes/Command Classes/Commands.cs
+++ ForkBot CORE/Classes/Command Classes/Commands.cs
@@ -2113,7 +2113,7 @@ namespace ForkBot
         [Command("talk"), Summary("Chat time with ForkBot.")]
         public async Task Talk([Remainder] string input = "")
         {
-            if (Context.Guild.Id != Constants.Guilds.YORK_UNIVERSITY || Context.User.Id == Constants.Users.BRADY)
+            if (Context.Guild.Id != Constants.Guilds.YORK_UNIVERSITY && Context.User.Id != Constants.Users.BRADY)
             {
                 await ReplyAsync(embed: new InfoEmbed("Sorry...", "This command is currently only available in the York University server.").Build());
                 return;
@@ -2221,7 +2221,7 @@ namespace ForkBot
         [Command("gpt")]
         public async Task GPT([Remainder] string input)
         {
-            if (Context.Guild.Id != Constants.Guilds.YORK_UNIVERSITY || Context.User.Id == Constants.Users.BRADY)
+            if (Context.Guild.Id != Constants.Guilds.YORK_UNIVERSITY && Context.User.Id != Constants.Users.BRADY)
             {
                 await ReplyAsync(embed: new InfoEmbed("Sorry...", "This command is currently only available in the York University server.").Build());
                 return;
